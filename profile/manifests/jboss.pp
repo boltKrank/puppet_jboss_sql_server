ee52de7f70fdc8700e7dd974d7c8646d99a29707
boltKrank <simon.anderson111@gmail.com>
2020-05-27 04:55:09 +1000

    Update jboss.pp

--- profile/manifests/jboss.pp
+++ profile/manifests/jboss.pp
@@ -1,8 +1,22 @@
 class profile::jboss {
 
-	package { 'jboss-as':
-		ensure => installed,
-		provider => chocolatey,
+	# from module puppetlabs-archive:
+
+	include 'archive'
+
+	archive { 'C:\temp\openjdk-14.0.1_windows-x64_bin.zip':
+		source => 'https://download.java.net/java/GA/jdk14.0.1/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-14.0.1_windows-x64_bin.zip',
+		extract => true,
+		extract_path => 'C:\temp',
+		creates => 'C:\temp\jdk-14.0.1',
+		cleanup => false,
 	}
+
+
+#	package { 'jboss-as':
+#		ensure => installed,
+#		provider => chocolatey,
+#		requires => Package['jdk11'],
+#	}
 
 }
